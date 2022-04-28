ef7431b825ea5c5539becb4bbd0da9a3b94c9dce
Dane-Shaut <dshaut211@stevenscollege.edu>
2022-04-28 10:14:49 -0400

    DBconnect
    connect and create to the database

--- BikeShopFinalProject/MyProject/main.py
+++ BikeShopFinalProject/MyProject/main.py
@@ -4,10 +4,43 @@ from flask import request
 app = Flask(__name__)
 import psycopg2
 
+def db_connect():
+    conn = psycopg2.connect(
+    host = 'localhost',
+    database = 'FinalBike',
+    )
+    return conn
 
 @app.route('/')
 def main():
     return render_template('StorePage.html')
+
+
+@app.route('/end')
+def end():
+    conn = db_connect()
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS Bikerev(
+                    Five_star varchar(500),
+                    Four_star varchar(500),
+                    Three_star varchar(500),
+                    Two_star varchar(500),
+                    One_star varchar(500) 
+                    )""")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('EndPointPage.html')
+
+# i dont even know
+# def review_table():
+    
+
+
+# Not yet done do not tocuh or remove this
+# @app.route('/end',methods=['POST', 'GET'])
+# def end1():
+#     pass
 
 
 
