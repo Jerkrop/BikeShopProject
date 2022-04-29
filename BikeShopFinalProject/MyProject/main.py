67ddb0ac75ac373fbb558e4ffc9f9768015bbb0b
Dane-Shaut <dshaut211@stevenscollege.edu>
2022-04-29 09:30:55 -0400

    db review insert
    this function is not yet done yet, gettin a bad key error.  Ther querys work when only one if statemnet is in.  Still working and testing it

--- BikeShopFinalProject/MyProject/main.py
+++ BikeShopFinalProject/MyProject/main.py
@@ -4,6 +4,7 @@ from flask import request
 app = Flask(__name__)
 import psycopg2
 
+# connects to the database
 def db_connect():
     conn = psycopg2.connect(
     host = 'localhost',
@@ -11,11 +12,13 @@ def db_connect():
     )
     return conn
 
+
+# connects to the mainpage
 @app.route('/')
 def main():
     return render_template('StorePage.html')
 
-
+# connect to the end page and adds the review table for the end page
 @app.route('/end')
 def end():
     conn = db_connect()
@@ -38,9 +41,54 @@ def end():
 
 
 # Not yet done do not tocuh or remove this
-# @app.route('/end',methods=['POST', 'GET'])
-# def end1():
-#     pass
+@app.route('/end',methods=['POST', 'GET'])
+def end1():
+    review = request.form['review']
+    five = request.form['five']
+    four = request.form['four']
+    three = request.form['three']
+    two = request.form['two']
+    one = request.form['one']
+    if request.form['five'] is five:
+        conn = db_connect()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO Bikerev (five_star) VALUES(%s)',(review,))
+        conn.commit()
+        cur.close()
+        conn.close()
+    elif request.form['four']is four:
+        conn = db_connect()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO Bikerev (four_star) VALUES(%s)',(review,))
+        conn.commit()
+        cur.close()
+        conn.close()
+    elif request.form['three'] is three:
+        conn = db_connect()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO Bikerev (three_star) VALUES(%s)',(review,))
+        conn.commit()
+        cur.close()
+        conn.close()
+    elif request.form['two'] is two:
+        conn = db_connect()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO Bikerev (two_star) VALUES(%s)',(review,))
+        conn.commit()
+        cur.close()
+        conn.close()
+    elif request.form['one'] is one:
+        conn = db_connect()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO Bikerev (one_star) VALUES(%s)',(review,))
+        conn.commit()
+        cur.close()
+        conn.close()
+    else:
+        return render_template(main)
+        
+    return render_template('EndPointPage.html')
+
 
 
 
