045b2a69a13dab1a7b239c22f83a1eb38e312674
Jeremy Kropa <arkedi487@gmail.com>
2022-05-11 10:54:40 -0400

    Fixed the form from  being in columns instead of rows added some css to sign up/login and admin page

--- BikeShopFinalProject/MyProject/main.py
+++ BikeShopFinalProject/MyProject/main.py
@@ -194,6 +194,10 @@ def road_bike():
 def SignIn():
     return render_template('Signin.html')
 
+@app.route('/AdminPage')
+def Admin():
+    return render_template('Signin.html')
+
 @app.route('/Register')## base route for register and adds an admin login
 def Register():
     conn = db_connect()
@@ -201,7 +205,7 @@ def Register():
     pasw = bytes('password', 'utf-8')
     hashed = bcrypt.hashpw(pasw, salt)
     hashed = hashed.decode('utf-8')
-    cur.execute('SELECT * FROM bicycle')
+    cur.execute('SELECT * FROM Bicycle')
     info = cur.fetchall()
     for i in range(0,len(info) + 1):
         if i == len(info):
